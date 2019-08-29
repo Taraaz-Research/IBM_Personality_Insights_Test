188160cdc0d96737dab13a8056d1dde5d2126551
royapakzad <roya.pakzad@gmail.com>
2019-08-28 20:53:28 -0700

    Delete json_cleaning.py

--- json_cleaning.py
+++
@@ -1,6 +0,0 @@
-# Cleaning Output json cleaning"""
-import json
-with open('output.json') as data_file:
-  parsed=json.load(data_file)
-with open ('output.json', 'w') as data_file:
-  json.dump(parsed, data_file, indent=4)
